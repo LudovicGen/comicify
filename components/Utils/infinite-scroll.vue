73bc3a53df9f91fd1764c6834996611e41b2b547
ludovicGendre <ludovic.gendre@g-mail.fr>
2022-04-21 11:54:48 +0200

    feat: add fetch and card character

---
+++ components/Utils/infinite-scroll.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-card
+    v-intersect="{
+      handler: onIntersect,
+      options: {
+        threshold: [0, 0.5, 1.0],
+      },
+    }"
+    class="text-center"
+    elevation="0"
+  >
+    <div class="font-weight-bold text-body-2">
+      <v-card-text v-if="loadingNewPage"
+        ><v-icon color="primary" large>fas fa-circle-notch fa-spin</v-icon>
+        <p>{{ `Affichage ${items.length} sur ${total} éléments` }}</p></v-card-text
+      >
+      <v-card-text v-else>{{ `${total} sur ${total} éléments` }}</v-card-text>
+    </div>
+  </v-card>
+</template>
+<script lang="ts">
+import { Component, Prop, VModel, Vue } from 'nuxt-property-decorator'
+import type { PropType } from 'vue/types/options'
+
+@Component({})
+export default class UtilsInfiniteScroll<Entity> extends Vue {
+  @VModel({ type: Array as PropType<Entity[]>, default: () => [] })
+  public items!: Entity[]
+
+  @Prop({ type: Number })
+  public total!: number
+
+  @Prop({ type: String })
+  public apiUrl!: string
+
+  public page = 0
+
+  public loadingNewPage = false
+
+  public async onIntersect(entries?: { intersectionRatio: number }[]): Promise<void> {
+    const isIntersecting = entries?.[0] ? entries[0].intersectionRatio >= 0.5 : false
+    if (isIntersecting && this.total > this.items.length && !this.loadingNewPage) {
+      this.loadingNewPage = true
+      const result = await this.loadNewPage()
+      if (result.length > 0) {
+        this.items.push(...result)
+      }
+      this.loadingNewPage = false
+    }
+  }
+
+  public async loadNewPage(): Promise<Entity[]> {
+    this.page = this.page + 20
+    const { data } = await this.$axios.$get<Entity[]>(this.apiUrl, {
+      params: { offset: this.page },
+    })
+    return data.results
+  }
+}
+</script>
---
+++ components/card.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card elevation="0" outlined height="100%">
+    <v-img
+      height="150"
+      :src="character.thumbnail.path + '.' + character.thumbnail.extension"
+    ></v-img>
+    <v-card-title class="font-weight-bold">{{ character.name }}</v-card-title>
+  </v-card>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+import { PropType } from 'vue'
+import { Character } from '@/utils/character'
+
+@Component({})
+export default class Card extends Vue {
+  @Prop({ type: Object as PropType<Character> })
+  public character!: Character
+}
+</script>
